695cc5bad66c3fe03086c4ab2b051ffdfff5dcb3
Redo76 <redouane.hamadache1@gmail.com>
2022-03-31 16:53:10 +0200

    finalisation de l'index en anglais

--- main.js
+++
@@ -1,9 +0,0 @@
-const uk = document.getElementById("#uk");
-const france = document.querySelector("#france");
-
-uk.addEventListener("click", (e) =>{
-    console.log(e.target)
-    if (uk){
-        france.classList.toggle(".show")
-    }
-})
